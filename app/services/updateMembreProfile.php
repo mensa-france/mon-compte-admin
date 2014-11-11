a28e562193c72025ef3f8ff8c72bfe0cab2270fd
Nicolas Leclerc <nl@spirotron.fr>
2014-11-11 14:33:31 +0100

    Added some member profile validation on save.

--- app/services/updateMembreProfile.php
+++ app/services/updateMembreProfile.php
@@ -5,6 +5,8 @@ require_once __DIR__.'/../../vendor/autoload.php';
 use MonCompte\Logger;
 use MonCompte\StopWatch;
 use MonCompte\DB\Queries;
+use MonCompte\LdapSync;
+use Valitron\Validator;
 
 $stopWatch = new StopWatch();
 $stopWatch->start();
@@ -20,11 +22,41 @@ $message = null;
 $numeroMembreStr = @$_GET['numero_membre'];
 $numeroMembre = @intval($numeroMembreStr);
 
+$v = new Validator($_GET);
+
+$v->rule('required',[
+	'adresse1',
+	'code_postal',
+	'ville',
+	'pays',
+])->message('{field} doit être renseigné.');
+
+$v->rule('email','email')->message('{field} n\'est pas une adresse email valide.');
+$v->rule('in','statut',[null,'','single','couple','deceased'])->message('{field} n\'est pas valide.');
+$v->rule('integer','enfants')->message('{field} n\'est pas un nombre entier.');
+
 if (!$numeroMembre) {
 	$errors[] = "Invalid value for numero_membre: {$numeroMembreStr}";
-} else {
-	$membreData = Queries::setMembreBaseData($numeroMembre, $_GET);
+} else if ($v->validate()) {
+	Queries::setMembreBaseData($numeroMembre, $_GET);
+	$membreData = Queries::findMembreBaseData($numeroMembre);
+
+	$ldapResult = LdapSync::updateOrCreateProfile($numeroMembre, $membreData);
+
+	if ($ldapResult) {
+		// Then it's an error.
+		$errors[] = "Ldap error updating status for #{$numeroMembre}: {$ldapResult}";
+	}
+
 	$message = 'Completed';
+} else {
+	foreach ($v->errors() as $fieldName => $fieldErrors) {
+		foreach ($fieldErrors as $message) {
+			$errors[] = $message;
+		}
+	}
+
+	$logger->info('Found validation errors: '.print_r($errors,true));
 }
 
 if (count($errors) > 0)

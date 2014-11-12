bdcd0d38c5e0cab15c876779450b8f0753cfa8d0
Nicolas Leclerc <nl@spirotron.fr>
2014-11-12 23:54:58 +0100

    Added some member profile field length limits.

--- app/services/updateMembreProfile.php
+++ app/services/updateMembreProfile.php
@@ -6,6 +6,7 @@ use MonCompte\Logger;
 use MonCompte\StopWatch;
 use MonCompte\DB\Queries;
 use MonCompte\LdapSync;
+use MonCompte\Format;
 use Valitron\Validator;
 
 $stopWatch = new StopWatch();
@@ -19,10 +20,11 @@ $errors = [];
 $response = [];
 $message = null;
 
-$numeroMembreStr = @$_GET['numero_membre'];
+$formValues = $_GET;
+$numeroMembreStr = @$formValues['numero_membre'];
 $numeroMembre = @intval($numeroMembreStr);
 
-$v = new Validator($_GET);
+$v = new Validator($formValues);
 
 $v->rule('required',[
 	'adresse1',
@@ -35,10 +37,24 @@ $v->rule('email','email')->message('{field} n\'est pas une adresse email valide.
 $v->rule('in','statut',[null,'','single','couple','deceased'])->message('{field} n\'est pas valide.');
 $v->rule('integer','enfants')->message('{field} n\'est pas un nombre entier.');
 
+$MAX_LENGTHS = [
+	'adresse1' => 35,
+	'adresse2' => 35,
+	'adresse3' => 35,
+	'code_postal' => 20,
+	'ville' => 50,
+	'pays' => 50,
+	'phone' => 20,
+	'email' => 100,
+];
+
 if (!$numeroMembre) {
 	$errors[] = "Invalid value for numero_membre: {$numeroMembreStr}";
 } else if ($v->validate()) {
-	Queries::saveMembreData($numeroMembre, $_GET);
+	foreach ($MAX_LENGTHS as $key => $value)
+		$formValues[$key] = Format::limitLength($formValues[$key], $MAX_LENGTHS[$key]);
+
+	Queries::saveMembreData($numeroMembre, $formValues);
 
 	$membreData = Queries::findMembreBaseData($numeroMembre);
 
